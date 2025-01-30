00d8c0c6823261ce977ca110c09d26a5589576d7
Attila <113335361+BencsikAttila@users.noreply.github.com>
2025-01-30 13:41:04 +0100

    Update mysql.sql
    fix

--- src/db/mysql.sql
+++ src/db/mysql.sql
@@ -3,8 +3,8 @@ CREATE DATABASE message_app;
 USE message_app;
 
 CREATE TABLE messages (
-	id bigint AUTO_INCREMENT NOT NULL,
+    id int AUTO_INCREMENT NOT NULL,
     content text NOT NULL,
-    createdUtc int NOT NULL,
+    createdUtc bigint NOT NULL,
     primary key (id)
 );
