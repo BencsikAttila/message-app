07e125e0b8dff0b7fd3951bee47f2f1bb21a8860
BB <bazsi5fel2@gmail.com>
2025-03-28 10:09:43 +0100

    finally

--- src/db/table.js
+++ src/db/table.js
@@ -5,6 +5,23 @@ const typeKeywords = {
     'VARCHAR': { mysql: 'VARCHAR', sqlite: 'VARCHAR' },
 }
 
+/**
+ * @typedef {{
+ *   setPrimary(): ColumnBuilder
+ *   setNotNull(): ColumnBuilder
+ *   setUnique(): ColumnBuilder
+ *   setAutoIncrement(): ColumnBuilder
+ * }} ColumnBuilder
+ */
+
+/**
+ * @param {string} tableName
+ * @returns {{
+ *   addColumn(name: string, type: 'INT' | 'TEXT' | 'BIGINT'): ColumnBuilder
+ *   addColumn(name: string, type: 'VARCHAR', length: number): ColumnBuilder
+ *   compile(protocol: 'sqlite' | 'mysql'): string
+ * }}
+ */
 module.exports = function(tableName) {
     const columns = {}
     const addColumn = (name, type, param) => {
@@ -17,12 +34,18 @@ module.exports = function(tableName) {
             isAutoIncrement: false,
         }
         columns[name] = column
-        return {
-            setPrimary() { column.isPrimary = true },
-            setNotNull() { column.isNotNull = true },
-            setUnique() { column.isUnique = true },
-            setAutoIncrement() { column.isAutoIncrement = true },
+        const columnBuilder = {
+            setPrimary() { column.isPrimary = true; return columnBuilder },
+            setNotNull() { column.isNotNull = true; return columnBuilder },
+            setUnique() { column.isUnique = true; return columnBuilder },
+            setAutoIncrement() { column.isAutoIncrement = true; return columnBuilder },
         }
+        return columnBuilder
     }
-    const compile = (protocol) => `CREATE TABLE ${tableName} (${Object.keys(columns).map(columnName => `${columnName} ${typeKeywords[columns[columnName].type][protocol]}${columns[columnName].param ? `(${columns[columnName].param})` : ''} ${columns[columnName].isPrimary ? ' PRIMARY KEY' : ''} ${columns[columnName].isNotNull ? ' NOT NULL' : ''} ${columns[columnName].isUnique ? ' UNIQUE' : ''} ${columns[columnName].isAutoIncrement ? ' ' + { mysql: 'AUTO_INCREMENT', sqlite: 'AUTOINCREMENT' }[protocol] : ''}`).join(', ')})`
+    const compile = (protocol) => `CREATE TABLE ${tableName} (${Object.keys(columns).map(columnName => `${columnName} ${typeKeywords[columns[columnName].type][protocol]}${columns[columnName].param ? `(${columns[columnName].param})` : ''}${columns[columnName].isPrimary ? ' PRIMARY KEY' : ''}${columns[columnName].isNotNull ? ' NOT NULL' : ''}${columns[columnName].isUnique ? ' UNIQUE' : ''}${columns[columnName].isAutoIncrement ? ' ' + { mysql: 'AUTO_INCREMENT', sqlite: 'AUTOINCREMENT' }[protocol] : ''}`).join(', ')})`
+
+    return {
+        addColumn,
+        compile,
+    }
 }
